cf9680f2a51f4e584f968e4a85ba6d9d690feb65
Ubuntu <ubuntu@ip-172-31-90-178.ec2.internal>
2020-08-22 12:56:15 +0000

    blog structure and code

---
+++ myblog/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Router from './components/Router';
+
+
+class App extends Component {
+  render() {
+    return (
+        <Router />
+    );
+  }
+}
+
+export default App;
